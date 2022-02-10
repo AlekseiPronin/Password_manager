6752a9fc22e676fd5f92b919f0e4bd6c7896a6be
Pronin <alexpronin.dm@gmail.com>
2022-02-10 16:39:37 +0300

    added write and read key functions

--- code.py
+++ code.py
@@ -1,9 +1,27 @@
+from cryptography.fernet import Fernet
+
 master_pwd = input('Input the master password: ')
+
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open("key.key", 'wb') as key_file:
+#         key_file.write(key)
+
+# write_key()
+
+
+def read_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
 
 def view():
     with open('passwords.txt', 'r') as file:
         for line in file.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            user, passw = data.split(' | ')
+            print('User: ', user, ', Password: ', passw)
 
 def add():
     name = input('Account Name: ')
